e344d3107ed871659844ac4fe81d223a635adb40
OlaPietka <36880623+OlaPietka@users.noreply.github.com>
2019-10-13 18:14:25 +0200

    Implement brute force algorithm

--- Nonogram/Program.cs
+++ Nonogram/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Nonogram
 {
@@ -6,32 +8,122 @@ namespace Nonogram
     {
         static void Main(string[] args)
         {
-            var n = 3;
-            var m = 5;
+            var boardValues = JsonHelper.ReadJsonFile("input.json");
 
-            var board = new Board(n, m);
-            var playerBoard = Helper.GenerateRandomGameBoard(n, m);
+            var min = int.MaxValue;
+            var finalBoard = new bool[boardValues.RowCount, boardValues.ColumnCount];
 
-            #region Visualisation
-            for (var i = 0; i < n; i++)
+            foreach (var board in GenerateCombinations(boardValues.RowCount, boardValues.ColumnCount))
             {
-                for (var j = 0; j < m; j++)
-                    Console.Write(board.GetCorrectBoard()[i, j] ? 'O' : 'X');
+                var errors = CheckForErrors(boardValues, board);
+
+                if (errors < min)
+                {
+                    min = errors;
+                    finalBoard = board;
+                }
+            }
+
+            for (var i = 0; i < finalBoard.GetLength(0); i++)
+            {
+                for (var j = 0; j < finalBoard.GetLength(1); j++)
+                    if (finalBoard[i, j])
+                        Console.Write('X');
+                    else
+                        Console.Write('O');
                 Console.Write("\n");
             }
 
-            Console.Write("\n");
+            JsonHelper.WriteJsonFile(finalBoard, min, "output.json");
 
-            for (var i = 0; i < n; i++)
-            {
-                for (var j = 0; j < m; j++)
-                    Console.Write(playerBoard[i, j] ? 'O' : 'X');
-                Console.Write("\n");
-            }
-            #endregion
-
-            Console.WriteLine(board.Check(playerBoard));
             Console.ReadKey();
+        }
+
+        private static int CheckForErrors(BoardValues boardValues, bool[,] filledBoard)
+        {
+            var errors = 0;
+
+            void CountErrors(List<int>[] correctNumbers, List<int>[] filledNumbers, int dimension)
+            {
+                for (var i = 0; i < filledBoard.GetLength(dimension); i++)
+                {
+                    var correctNumbersCount = correctNumbers[i].Count;
+                    var filedNumbersCount = filledNumbers[i].Count;
+
+                    if (correctNumbersCount == filedNumbersCount)
+                    {
+                        foreach (var (Correct, Filled) in correctNumbers[i].Zip(filledNumbers[i], (c, f) => (Correct: c, Filled: f)))
+                            if (Correct != Filled)
+                                errors += Math.Abs(Correct - Filled);
+                    }
+                    else if (correctNumbersCount == 0 ^ filedNumbersCount == 0)
+                        foreach (var x in correctNumbersCount == 0 ? filledNumbers[i] : correctNumbers[i])
+                            errors += x;
+                    else
+                    {
+                        (List<int> longerList, int shorterListCount) = correctNumbersCount < filedNumbersCount ?
+                            (filledNumbers[i], correctNumbersCount) : (correctNumbers[i], filedNumbersCount);
+
+                        for (var j = 0; j < shorterListCount; j++)
+                            if (correctNumbers[i][j] != filledNumbers[i][j])
+                                errors += Math.Abs(correctNumbers[i][j] - filledNumbers[i][j]);
+
+                        longerList.GetRange(shorterListCount, longerList.Count - shorterListCount).ForEach(x => {
+                            errors += x;
+                        });
+                    }
+                }
+            }
+
+            var filledRowsNumbers = BoardHelper.GenerateNumbers(filledBoard, dimension: 0);
+            var filledColumnsNumbers = BoardHelper.GenerateNumbers(filledBoard, dimension: 1);
+
+            CountErrors(boardValues.RowsNumbers, filledRowsNumbers, dimension: 0);
+            CountErrors(boardValues.ColumnsNumbers, filledColumnsNumbers, dimension: 1);
+
+            return errors;
+        }
+
+        private static List<bool[,]> GenerateCombinations(int rows, int columns)
+        {
+            IEnumerable<int> Generator(int limit, int duplicates)
+            {
+                while (true)
+                {
+                    for (var i = 0; i < limit; i++)
+                    {
+                        for (var c = 0; c < duplicates; c++)
+                            yield return i;
+                    }
+                }
+            }
+
+            var valueLimit = (int)Math.Pow(2, columns);
+            var combinations = (int)Math.Pow(valueLimit, rows);
+
+            var enumerators = new List<IEnumerator<int>>();
+            for (var i = 0; i < rows; i++)
+                enumerators.Add(Generator(valueLimit, (int)Math.Pow(valueLimit, rows - i - 1)).GetEnumerator());
+
+            var result = new List<bool[,]>();
+            for (var i = 0; i < combinations; i++)
+            {
+                var t = new bool[rows, columns];
+
+                for (var y = 0; y < rows; y++)
+                {
+                    var e = enumerators[y];
+                    e.MoveNext();
+                    var current = e.Current;
+                    for (var x = 0; x < columns; x++)
+                        t[y, x] = ((current >> (columns - x - 1)) & 1) == 1;
+
+                }
+
+                result.Add(t);
+            }
+
+            return result;
         }
     }
 }
