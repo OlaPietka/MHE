37a84492caca32a288ff52ac2482354cabf1c9d1
OlaPietka <36880623+OlaPietka@users.noreply.github.com>
2019-10-13 18:11:57 +0200

    New model for board values

---
+++ Nonogram/BoardValues.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Nonogram
+{
+    public class BoardValues
+    {
+        public int RowCount { get; set; }
+        public int ColumnCount { get; set; }
+        public List<int>[] RowsNumbers { get; set; }
+        public List<int>[] ColumnsNumbers { get; set; }
+    }
+}
