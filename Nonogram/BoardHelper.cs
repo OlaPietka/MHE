e0b0b8cd64afa405e1a72e6e54281c293c7da055
Ola Piętka <s17611@pjwstk.edu.pl>
2019-10-23 22:01:37 +0200

    Separate generate methods

--- Nonogram/BoardHelper.cs
+++ Nonogram/BoardHelper.cs
@@ -1,58 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Nonogram
 {
     public static class BoardHelper
     {
-        private static readonly Random _random = new Random();
-
-        public static bool[,] GenerateRandomBoard(int n, int m)
+        public static int CheckForErrors(BoardValues boardValues, bool[,] filledBoard)
         {
-            var board = new bool[n, m];
+            var errors = 0;
 
-            for (var i = 0; i < n; i++)
-                for (var j = 0; i < m; j++)
-                    board[i, j] = _random.Next(0, 10) % 2 != 0;
-
-            return board;
-        }
-
-        public static List<int>[] GenerateNumbers(bool[,] board, int dimension)
-        {
-            var n = board.GetLength(0);
-            var m = board.GetLength(1);
-
-            (var x, var y) = dimension == 0 ? (n, m) : (m, n);
-
-            var counter = 0;
-            var numbers = new List<int>[x];
-
-            for (var i = 0; i < x; i++)
-                numbers[i] = new List<int>();
-
-            for (var i = 0; i < x; i++)
+            void CountErrors(List<int>[] correctNumbers, List<int>[] filledNumbers, int dimension)
             {
-                for (var j = 0; j < y; j++)
+                for (var i = 0; i < filledBoard.GetLength(dimension); i++)
                 {
-                    if (dimension == 0 ? board[i, j] : board[j, i])
-                    {
-                        counter++;
+                    var correctNumbersCount = correctNumbers[i].Count;
+                    var filedNumbersCount = filledNumbers[i].Count;
 
-                        if (j == y - 1)
-                            numbers[i].Add(counter);
+                    if (correctNumbersCount == filedNumbersCount)
+                    {
+                        foreach (var (Correct, Filled) in correctNumbers[i].Zip(filledNumbers[i], (c, f) => (Correct: c, Filled: f)))
+                            if (Correct != Filled)
+                                errors += Math.Abs(Correct - Filled);
                     }
+                    else if (correctNumbersCount == 0 ^ filedNumbersCount == 0)
+                        foreach (var x in correctNumbersCount == 0 ? filledNumbers[i] : correctNumbers[i])
+                            errors += x;
                     else
                     {
-                        if (counter > 0)
-                            numbers[i].Add(counter);
-                        counter = 0;
+                        (List<int> longerList, int shorterListCount) = correctNumbersCount < filedNumbersCount ?
+                            (filledNumbers[i], correctNumbersCount) : (correctNumbers[i], filedNumbersCount);
+
+                        for (var j = 0; j < shorterListCount; j++)
+                            if (correctNumbers[i][j] != filledNumbers[i][j])
+                                errors += Math.Abs(correctNumbers[i][j] - filledNumbers[i][j]);
+
+                        longerList.GetRange(shorterListCount, longerList.Count - shorterListCount).ForEach(x => {
+                            errors += x;
+                        });
                     }
                 }
-                counter = 0;
             }
 
-            return numbers;
+            var filledRowsNumbers = Generator.GenerateNumbers(filledBoard, dimension: 0);
+            var filledColumnsNumbers = Generator.GenerateNumbers(filledBoard, dimension: 1);
+
+            CountErrors(boardValues.RowsNumbers, filledRowsNumbers, dimension: 0);
+            CountErrors(boardValues.ColumnsNumbers, filledColumnsNumbers, dimension: 1);
+
+            return errors;
+        }
+
+        public static List<bool[,]> GetNeighbours(bool[,] currentBoard)
+        {
+            var neighbours = new List<bool[,]>();
+
+            for (var i = 0; i < currentBoard.GetLength(0); i++)
+                for (var j = 0; j < currentBoard.GetLength(1); j++)
+                {
+                    var newBoard = currentBoard.Clone() as bool[,];
+                    newBoard[i, j] = !newBoard[i, j];
+                    neighbours.Add(newBoard);
+                }
+
+            return neighbours;
+        }
+
+        public static void Print(Result result, string someNote = "")
+        {
+            Console.WriteLine(someNote);
+            for (var i = 0; i < result.Board.GetLength(0); i++)
+            {
+                for (var j = 0; j < result.Board.GetLength(1); j++)
+                    if (result.Board[i, j])
+                        Console.Write('X');
+                    else
+                        Console.Write('O');
+                Console.Write("\n");
+            }
+            Console.WriteLine($"Error: {result.Error}\n");
         }
     }
 }
