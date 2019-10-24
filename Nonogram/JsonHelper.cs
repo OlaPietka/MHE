4c53329a33dbdeaf498f45764a1d2aa7f536843c
Ola Piętka <s17611@pjwstk.edu.pl>
2019-10-24 14:36:11 +0200

    Code clean up

--- Nonogram/JsonHelper.cs
+++ Nonogram/JsonHelper.cs
@@ -18,33 +18,15 @@ namespace Nonogram
             return JsonConvert.DeserializeObject<BoardValues>(json);
         }
 
-        public static void WriteJsonFile(Result result, float time, string fileName)
+        public static void WriteJsonFile(Result result, string fileName)
         {
             var path = Path.Combine(Directory.GetCurrentDirectory(), fileName);
 
             using (var file = File.CreateText(path))
-            using (var writer = new JsonTextWriter(file))
             {
-                writer.Formatting = Formatting.Indented;
-
-                writer.WriteStartObject();
-                writer.WritePropertyName("Errors");
-                writer.WriteValue(result.Error);
-                writer.WritePropertyName("Time");
-                writer.WriteValue(time);
-                writer.WritePropertyName("Board");
-                writer.WriteStartArray();
-
-                for (var i = 0; i < result.Board.GetLength(0); i++)
-                {
-                    writer.WriteStartArray();
-                    for (var j = 0; j < result.Board.GetLength(0); j++)
-                        writer.WriteValue(result.Board[i, j]);
-                    writer.WriteEnd();
-                }
-
-                writer.WriteEnd();
-                writer.WriteEndObject();
+                var serializer = new JsonSerializer();
+                serializer.Formatting = Formatting.Indented; 
+                serializer.Serialize(file, result);
             }
         }
     }
